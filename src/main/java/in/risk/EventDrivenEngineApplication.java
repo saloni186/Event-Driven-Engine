401c8d3d87dbbdccea3faeb2907cfed3502c4307
saloni186 <salonijaiswal390@gmail.com>
2026-05-09 12:54:38 +0530

    Converted project to Spring Boot and added gitignore

---
+++ src/main/java/in/risk/EventDrivenEngineApplication.java
@@ -0,0 +1,12 @@
+package in.risk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenEngineApplication.class, args);
+    }
+}
--- src/main/java/in/risk/RiskApplication.java
+++
@@ -1,7 +0,0 @@
-package in.risk;
-
-public class RiskApplication {
-	public static void main(String[] args) {
-		System.out.println("Main class");
-	}
-}
